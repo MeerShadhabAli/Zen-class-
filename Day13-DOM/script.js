f22e9b7046d4401ba926f6d4948f00f5c36c2937
Meershadhap04 <meershadhapali04@gmail.com>
2023-07-04 22:29:14 +0530

    Zen class

---
+++ Day13-DOM/script.js
@@ -0,0 +1,11 @@
+let heading = document.querySelector('h1');
+
+// console.log(heading.textContent);
+let paragraphs = document.querySelectorAll('#firstDiv p');
+
+
+let paragraphs=document.querySelectorAll('.red');
+
+//paragraphs.forEach(para => console.log(para.textcontent));
+
+console.log(paragraphs[0].textContent);
