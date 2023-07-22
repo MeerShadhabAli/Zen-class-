212e69603ef5284870b17fbe5be8d601246aef3e
Meershadhap04 <meershadhapali04@gmail.com>
2023-07-22 16:53:59 +0530

    Completed

---
+++ Day-15-domrecap/script.js
@@ -0,0 +1,33 @@
+
+
+// let spanColor = document.getElementById('spanColor');
+
+// spanColor.addEventListener('mouseenter',function (event){
+//     console.log(event);
+//     spanColor.style.color = 'blue';
+
+
+// });
+// spanColor.addEventListener('mouseout',fuction () {
+
+//     spanColor.style.color ='black';
+// });
+
+// function showCoordinates(events){
+//     let header = document.getElementById('heasder');
+//     let x = event.clientX;
+//     let y = event.clientY;
+
+//     //get the reference of span
+//     let spanCoordinates = document.getElementById('coordinates');
+
+//     spanCoordinates.textContent = 'coordinates x: ${x} and y: ${y} ';
+
+// }
+
+
+function characterCount() {
+    let textarea = document.getElementById('textarea');
+
+    console.log(textarea.Value.length);
+}
