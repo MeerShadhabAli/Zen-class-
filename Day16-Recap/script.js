eddc8ef17f10e6180d54b1e473dceba6c0082680
Meershadhap04 <meershadhapali04@gmail.com>
2023-07-24 23:14:08 +0530

    Completed

---
+++ Day16-Recap/script.js
@@ -0,0 +1,10 @@
+// // create a div 
+
+// // console.log('hello');
+// let containerDIV = document.createElement('div');
+
+// containerDIV.setAttribute('class','container');
+
+// // add it to the body element 
+
+// document.body.appendChild(containerDIV);
